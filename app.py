67fd686a732998967bcd9d3582ba8ed9e17d19dd
Jkb <jkb@jkbworld.com>
2017-02-09 23:40:34 -0800

    Added MongoEngine ODM to DurkaDurka Service

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
 #!flask/bin/python
 
 from flask import Flask, jsonify
-from bson import ObjectId
-from flask_pymongo import PyMongo
+from bson import ObjectId, json_util
+# from flask_pymongo import PyMongo
 from flask import request, abort
-
+from flask_mongoengine import MongoEngine, Document, DynamicDocument
+from mongoengine.fields import StringField
 
 app = Flask(__name__)
-app.config["MONGO_DBNAME"] = "durkadurka"
-mongo = PyMongo(app)
+#app.config["MONGO_DBNAME"] = "durkadurka"
+# mongo = PyMongo(app)
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'durkadurka'
+}
+mongo = MongoEngine(app)
+mongo.connect()
 
+
+class DurkaDurka(DynamicDocument):
+    meta = {
+        'collection': 'dd'
+    }
+    _id = StringField()
+    durka1 = StringField()
+    durka2 = StringField()
+
+    def display(self):
+        print "ID: {}, DD1: {}, DD2: {}".format(self._id, self.durka1, self.durka2)
 
 #
 # Default route
@@ -24,28 +41,39 @@ def index():
 #
 @app.route('/ddapi/v1.0/durkadurka', methods=['GET'])
 def get_dds():
-    ret = []
-    for dd in mongo.db.dd.find({}, {'durka1': 1, 'durka2': 1, '_id': 1}):
-        print "DD: {}".format(dd)
-        dd['_id'] = str(dd['_id'])
-        ret.append(dd)
+    # ret = []
+    # for dd in mongo.dd.find({}, {'durka1': 1, 'durka2': 1, '_id': 1}):
+    #     print "DD: {}".format(dd)
+    #     dd['_id'] = str(dd['_id'])
+    #     ret.append(dd)
+    #
+    # return jsonify(ret)
 
-    return jsonify(ret)
+    dd = DurkaDurka.objects()
+    for d in dd:
+        #print "ID: {}, DD1: {}, DD2: {}".format(d._id, d.durka1, d.durka2)
+        d.display()
 
+    # return json_util.dumps(dd._collection_obj.find(dd._query))
+    return jsonify(dd)
 
 #
 # Return a specific DurkaDurka given an id.
 #
 @app.route('/ddapi/v1.0/durkadurka/<string:dd_id>', methods=['GET'])
 def get_one_dd(dd_id):
-    ret = []
-    for dd in mongo.db.dd.find({'_id': ObjectId(dd_id)}, {'durka1': 1, 'durka2': 1, '_id': 1}):
-        print "DD: {}".format(dd)
-        dd['_id'] = str(dd['_id'])
-        ret.append(dd)
-
-    return jsonify(ret)
-
+    # ret = []
+    # for dd in mongo.db.dd.find({'_id': ObjectId(dd_id)}, {'durka1': 1, 'durka2': 1, '_id': 1}):
+    #     print "DD: {}".format(dd)
+    #     dd['_id'] = str(dd['_id'])
+    #     ret.append(dd)
+    #
+    # return jsonify(ret)
+    d = DurkaDurka.objects.get(_id=ObjectId(dd_id))
+    #for d in dd:
+    #print "ID: {}, DD1: {}, DD2: {}".format(d._id, d.durka1, d.durka2)
+    d.display()
+    return jsonify(d)
 
 #
 # Route and Function to delete the DurkaDurka given a DurkaDurka id.
